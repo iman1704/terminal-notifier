610e7672847be97bf2fdb121a3fa40c1cc62a491
iman1704 <mhdiman17@gmail.com>
2026-06-15 16:12:16 +0800

    Revert "removed placeholder file"
    This reverts commit 7d7d33b930da8df7ee436a9586a35ba1556e56c0.

---
+++ terminal-notifier.rb
@@ -0,0 +1,23 @@
+class TerminalNotifier < Formula
+  desc "Post native macOS User Notifications from the command-line"
+  homepage "https://github.com/mohamadiman/terminal-notifier"
+  url "file:///Users/mohamadiman/Documents/Projects/scratchpad"
+  version "2.0.0"
+  # Since it's a local file URL, we can use a dummy SHA or omit it
+  # sha256 "..."
+
+  def install
+    # Build the application bundle using our build script
+    system "./build.sh"
+
+    # Install the app bundle under the Homebrew libexec folder
+    libexec.install "terminal-notifier.app"
+
+    # Symlink the bundle's internal binary to Homebrew's bin folder
+    bin.install_symlink libexec/"terminal-notifier.app/Contents/MacOS/terminal-notifier" => "terminal-notifier"
+  end
+
+  test do
+    assert_match "2.0.0", shell_output("#{bin}/terminal-notifier -version")
+  end
+end
